548e072c9a22ddf5158a00ec424a596df4469758
Raphaël Stefanini <rphl@rphl.net>
2014-03-19 16:29:54 +0100

    Bug Fixed

--- seuranta/views.py
+++ seuranta/views.py
@@ -117,7 +117,7 @@ def api(request):
                 section.route = route
                 section.save()
 
-            return HttpResponse("OK %r"%route)
+            return HttpResponse("OK")
         return HttpResponse("Invalid parameters")
     else:
         action = request.GET.get('action')

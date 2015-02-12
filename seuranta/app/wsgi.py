d84599f884d2f4534345d0bb05f79c447109747f
Raphaël Stefanini <raphael.stefanini@qvantel.com>
2015-02-12 17:11:35 +0200

    add uwsgi

--- seuranta/app/wsgi.py
+++ seuranta/app/wsgi.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seuranta.app.settings")
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'seuranta.app.settings'
 application = get_wsgi_application()

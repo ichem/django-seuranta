f73f9309675758048862ff1b56f887679925a819
Raphaël Stefanini <rphlo@users.noreply.github.com>
2017-06-14 03:18:08 +0300

    Create race_viewer_app.js

--- seuranta/static/seuranta/js/race_viewer_app.js
+++ seuranta/static/seuranta/js/race_viewer_app.js
@@ -355,7 +355,7 @@ $(function() {
     center: [15, 0],
     zoom: 3
   });
-  open_street_map = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+  open_street_map = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
   });
